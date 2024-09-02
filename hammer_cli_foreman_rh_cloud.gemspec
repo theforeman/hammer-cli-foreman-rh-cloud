theforeman/hammer-cli-foreman-rh-cloud

commit a1ffa5f461822adeff8541bb60b51ec86bd31820

Add ruby support '>= 2.7', '< 4'

hammer_cli_foreman_rh_cloud.gemspec
@@ -18,4 +18,6 @@ Gem::Specification.new do |spec|
   spec.test_files    = Dir['{test}/**/*']
 
   spec.add_dependency 'hammer_cli_foreman', '>= 3.0.0', '< 4.0.0'
+
+  s.required_ruby_version = '>= 2.7', '< 4'
 end

.rubocop.yml
@@ -2,6 +2,9 @@ inherit_gem:
   theforeman-rubocop:
     - default.yml
 
+AllCops:
+  TargetRubyVersion: 2.7
+
 Style/FrozenStringLiteralComment:
   Enabled: false
 

commit 75604d699e758b391be8d9ab8c18ebeebccdfc79

Use the correct variable in required_ruby_version
The variable s doesn't exist and needs to be spec. This slipped by
because there was no CI set up in the repository.

Fixes: a1ffa5f46182 ("Add ruby support '>= 2.7', '< 4'")

hammer_cli_foreman_rh_cloud.gemspec
@@ -19,5 +19,5 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency 'hammer_cli_foreman', '>= 3.0.0', '< 4.0.0'
 
-  s.required_ruby_version = '>= 2.7', '< 4'
+  spec.required_ruby_version = '>= 2.7', '< 4'
 end