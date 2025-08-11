acf5b642f77c4edf6627fd9a5ed78a73129fff24
Lucy Fu <lufu@redhat.com>
2025-08-11 20:57:05 +0000

    SAT #36517 - Add foreman_rh_cloud to hammer ping

--- lib/hammer_cli_foreman_rh_cloud.rb
+++ lib/hammer_cli_foreman_rh_cloud.rb
@@ -5,6 +5,7 @@ module HammerCLIForemanRhCloud
   require 'hammer_cli_foreman_rh_cloud/version'
   require 'hammer_cli_foreman_rh_cloud/i18n'
   require 'hammer_cli_foreman_rh_cloud/insights'
+  require 'hammer_cli_foreman_rh_cloud/command_extensions/ping'
 
   HammerCLI::MainCommand.lazy_subcommand(
     'insights',
